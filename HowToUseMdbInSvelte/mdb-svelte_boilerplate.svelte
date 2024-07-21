82d4c29c4c71318c4146a10ba70806e9adcd672d
boonecabaldev <164784238+boonecabaldev@users.noreply.github.com>
2024-07-21 05:40:43 -0600

    Create mdb-svelte_boilerplate.svelte

---
+++ HowToUseMdbInSvelte/mdb-svelte_boilerplate.svelte
@@ -0,0 +1,71 @@
+<script>
+
+</script>
+
+<svelte:head>
+	
+	<!-- Bootstrap CSS -->
+	<link 
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+		rel="stylesheet" 
+		integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+		crossorigin="anonymous"
+	/>
+
+	<!-- Font Awesome -->
+	<link
+	  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+	  rel="stylesheet"
+	/>
+	
+	<!-- Google Fonts -->
+	<link
+	  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+	  rel="stylesheet"
+	/>
+	
+	<!-- MDB -->
+	<link
+	  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.3.2/mdb.min.css"
+	  rel="stylesheet"
+	/>
+
+</svelte:head>
+
+<body>
+
+	<!-- header -->
+	<header>
+		
+		<!-- navbar -->
+
+		<!-- navbar -->
+		
+	</header>
+	<!-- header -->
+
+	<!-- main -->
+	<main class="container-fluid ms-2 ms-sm-2 ms-md-3 ms-lg-4 mt-2 mt-sm-2 mt-md-3 mt-lg-4">
+
+
+
+	</main>
+	<!-- main -->
+	
+	<!--  Bootstrap Bundle with Popper -->
+	<script 
+		src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+		integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
+		crossorigin="anonymous"></script>
+	
+	<!-- MDB -->
+	<script
+	  type="text/javascript"
+	  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.3.2/mdb.umd.min.js"
+	></script>
+	
+</body>
+
+<style>
+	/* Scripts */
+</style>
