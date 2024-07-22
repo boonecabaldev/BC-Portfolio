46fe15816865320e927616477bd2548d1d6e274f
boonecabaldev <164784238+boonecabaldev@users.noreply.github.com>
2024-07-22 07:05:49 -0600

    Create Panel3.svelte

---
+++ HowToUseMdbInSvelte/CleaningUpCode/Panel3.svelte
@@ -0,0 +1,76 @@
+<script>
+
+</script>
+
+<section class="">
+	<div class="row gx-lg-5">
+		<div class="col-lg-6 mb-5">
+			<div class="d-flex align-items-start">
+				<div class="flex-shrink-0">
+					<div class="p-3 badge-primary rounded-4">
+						<i class="fas fa-cloud-upload-alt fa-lg text-primary fa-fw"></i>
+					</div>
+				</div>
+				<div class="flex-grow-1 ms-4">
+					<p class="fw-bold mb-1">Tutorials</p>
+					<p class="text-muted mb-1">
+						Dozens of free tutorials to help you discover the full potential of MDB.
+					</p>
+					<small><a href="https://mdbootstrap.com/docs/standard/components/badges/">Learn more</a></small>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-lg-6 mb-5">
+			<div class="d-flex align-items-start">
+				<div class="flex-shrink-0">
+					<div class="p-3 badge-primary rounded-4">
+						<i class="fas fa-database fa-lg text-primary fa-fw"></i>
+					</div>
+				</div>
+				<div class="flex-grow-1 ms-4">
+					<p class="fw-bold mb-1">Integrations</p>
+					<p class="text-muted mb-1">
+						MDB is integrated with all major technologies and tools.
+					</p>
+					<small><a href="https://mdbootstrap.com/docs/standard/components/badges/">Learn more</a></small>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-lg-6 mb-5">
+			<div class="d-flex align-items-start">
+				<div class="flex-shrink-0">
+					<div class="p-3 badge-primary rounded-4">
+						<i class="fas fa-stream fa-lg text-primary fa-fw"></i>
+					</div>
+				</div>
+				<div class="flex-grow-1 ms-4">
+					<p class="fw-bold mb-1">Backend friendly</p>
+					<p class="text-muted mb-1">
+						MDB is designed to be seamlessly integrated and used with the backend.
+					</p>
+					<small><a href="https://mdbootstrap.com/docs/standard/components/badges/">Learn more</a></small>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-lg-6 mb-5">
+			<div class="d-flex align-items-start">
+				<div class="flex-shrink-0">
+					<div class="p-3 badge-primary rounded-4">
+						<i class="fas fa-copy fa-lg text-primary fa-fw"></i>
+					</div>
+				</div>
+				<div class="flex-grow-1 ms-4">
+					<p class="fw-bold mb-1">Support and community</p>
+					<p class="text-muted mb-1">
+						The MDB team and our global community are there to support you.
+					</p>
+					<small><a href="https://mdbootstrap.com/docs/standard/components/badges/">Learn more</a></small>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</section>
